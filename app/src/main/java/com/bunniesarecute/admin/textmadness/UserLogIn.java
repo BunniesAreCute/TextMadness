d4009e9235c7f677cab2abb6da602e0ab5513b09
Brandy Foster <brandy.foster@detroitlabs.com>
2014-10-30 12:18:26 -0400

    added fragment for user log in and refactored fragment for user sign up

--- app/src/main/java/com/bunniesarecute/admin/textmadness/UserLogIn.java
+++ app/src/main/java/com/bunniesarecute/admin/textmadness/UserLogIn.java
@@ -13,13 +13,13 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link UserSignUpOrLogIn.OnFragmentInteractionListener} interface
+ * {@link UserLogIn.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link UserSignUpOrLogIn#newInstance} factory method to
+ * Use the {@link UserLogIn#newInstance} factory method to
  * create an instance of this fragment.
  *
  */
-public class UserSignUpOrLogIn extends Fragment {
+public class UserLogIn extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -37,18 +37,18 @@ public class UserSignUpOrLogIn extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment UserSignUpOrLogIn.
+     * @return A new instance of fragment UserLogIn.
      */
     // TODO: Rename and change types and number of parameters
-    public static UserSignUpOrLogIn newInstance(String param1, String param2) {
-        UserSignUpOrLogIn fragment = new UserSignUpOrLogIn();
+    public static UserLogIn newInstance(String param1, String param2) {
+        UserLogIn fragment = new UserLogIn();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
-    public UserSignUpOrLogIn() {
+    public UserLogIn() {
         // Required empty public constructor
     }
 
@@ -65,7 +65,7 @@ public class UserSignUpOrLogIn extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_sign_up_or_log_in, container, false);
+        return inflater.inflate(R.layout.fragment_user_log_in, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
