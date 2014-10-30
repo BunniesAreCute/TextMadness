bfb24ff6dc14641864b1379db8c7f07a13a06cbc
Brandy Foster <brandy.foster@detroitlabs.com>
2014-10-30 17:46:43 -0400

    added method to get the data

--- app/src/main/java/com/bunniesarecute/admin/textmadness/FireBaseMessages.java
+++ app/src/main/java/com/bunniesarecute/admin/textmadness/FireBaseMessages.java
@@ -1,6 +1,17 @@
 package com.bunniesarecute.admin.textmadness;
 
+import android.util.Log;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by BFineRocks on 10/30/14.
@@ -8,12 +19,56 @@ import com.firebase.client.Firebase;
 public class FireBaseMessages {
 
     final static String FIREBASE_BASE_URL = "https://dazzling-heat-5107.firebaseio.com/Users";
+    final static String FIREBASE_MESSAGE = "sentMessages";
+    final static String FIREBASE_TAG = "message";
+    final static ArrayList<String> messageHistory = new ArrayList<String>();
+
 
 
     public void addMessageToFireBase(String userName, String message)
     {
         Firebase ref = new Firebase(FIREBASE_BASE_URL);
         Firebase userRef = ref.child(userName);
-        userRef.push().setValue(message);
+        Firebase messageRef = userRef.child(FIREBASE_MESSAGE);
+        Map<String, String> messageMaker = new HashMap<String, String>();
+        messageMaker.put(FIREBASE_TAG, message);
+        messageRef.push().setValue(messageMaker);
+    }
+
+    public void getMessagesFromFireBase(String userName){
+        Firebase ref = new Firebase(FIREBASE_BASE_URL);
+        Firebase userRef = ref.child(userName);
+        Firebase messageRef = userRef.child(FIREBASE_MESSAGE);
+        Query messageQuery = messageRef.limit(10);
+        messageRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map<String, Object> message = (Map<String, Object>) dataSnapshot.getValue();
+                messageHistory.add(message.get(FIREBASE_TAG).toString());
+                Log.i("messageHistory", messageHistory.toString());
+            }
+
+/*            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }*/
+        });
+
     }
 }
