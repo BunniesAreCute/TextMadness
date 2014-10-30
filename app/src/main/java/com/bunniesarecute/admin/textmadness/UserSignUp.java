6d83e6724f0bad3a316fa97ae1b4f09a90edc666
Brandy Foster <brandy.foster@detroitlabs.com>
2014-10-30 12:31:57 -0400

    added string resources for user account

--- app/src/main/java/com/bunniesarecute/admin/textmadness/UserSignUp.java
+++ app/src/main/java/com/bunniesarecute/admin/textmadness/UserSignUp.java
@@ -65,7 +65,7 @@ public class UserSignUp extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_sign_up_or_log_in, container, false);
+        return inflater.inflate(R.layout.fragment_user_sign_up, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
