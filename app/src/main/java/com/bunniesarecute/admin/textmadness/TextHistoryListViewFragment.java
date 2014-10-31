eb6004909b8210ea8e9c88bc2994d2f1b4f8d557
Ken Luk <ken.luk@detroitlabs.com>
2014-10-30 23:22:09 -0400

    temp fix for message history

--- app/src/main/java/com/bunniesarecute/admin/textmadness/TextHistoryListViewFragment.java
+++ app/src/main/java/com/bunniesarecute/admin/textmadness/TextHistoryListViewFragment.java
@@ -39,7 +39,9 @@ public class TextHistoryListViewFragment extends Fragment {
         mFireBaseMessages.getMessagesFromFireBase(mSharedPreferences.getString("userName", null));
         mMessagesList = (ArrayList<String>) mFireBaseMessages.getArrayListOfMessages();
 
-        mMessageHistoryAdapter = new ArrayAdapter<String>(getActivity(), R.layout.text_history_listview, R.id.message_list_view);
+
+
+        mMessageHistoryAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mMessagesList);
         mMessageHistoryAdapter.addAll(mMessagesList);
         final ListView historyList = (ListView) v.findViewById(R.id.message_list_view);
 
@@ -47,7 +49,12 @@ public class TextHistoryListViewFragment extends Fragment {
 //        TextView messageTextView = (TextView)v.findViewById(R.id.textView);
 //        messageTextView.setText(message);
 
-
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mMessageHistoryAdapter.notifyDataSetChanged();
         return v;
     }
 
