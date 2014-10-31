2729f9281965e4f2d645c0350bbd75fca1e8cc64
Ken Luk <ken.luk@detroitlabs.com>
2014-10-30 20:14:35 -0400

    Tabbed ViewPager launches

---
+++ app/src/main/java/com/bunniesarecute/admin/textmadness/MainFragment.java
@@ -0,0 +1,33 @@
+package com.bunniesarecute.admin.textmadness;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment {
+
+    public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
+
+    public static final MainFragment newInstance(String message)
+    {
+        MainFragment f = new MainFragment();
+        Bundle bdl = new Bundle(1);
+        bdl.putString(EXTRA_MESSAGE, message);
+        f.setArguments(bdl);
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        String message = getArguments().getString(EXTRA_MESSAGE);
+        View v = inflater.inflate(R.layout.myfragment_layout, container, false);
+        TextView messageTextView = (TextView)v.findViewById(R.id.textView);
+        messageTextView.setText(message);
+
+        return v;
+    }
+
+}
---
+++ app/src/main/java/com/bunniesarecute/admin/textmadness/MainViewPagerActivity.java
@@ -0,0 +1,115 @@
+package com.bunniesarecute.admin.textmadness;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.FragmentTransaction;
+import android.app.ActionBar.Tab;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+public class MainViewPagerActivity extends FragmentActivity implements ActionBar.TabListener {
+
+    String[] tabTitles = new String[] {"Mad Text", "Text History"};
+    ActionBar actionBar;
+    ViewPager pager;
+
+    MyPageAdapter pageAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_view_pager);
+
+        List<Fragment> fragments = getFragments();
+
+        pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
+
+        pager = (ViewPager)findViewById(R.id.viewpager);
+        pager.setAdapter(pageAdapter);
+
+        actionBar = getActionBar();
+        actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Adding Tabs
+        for (String tab_name : tabTitles) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }
+
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page
+                // make respected tab selected
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+
+    }
+
+    private List<Fragment> getFragments(){
+        List<Fragment> fList = new ArrayList<Fragment>();
+//
+//        fList.add(MyFragment.newInstance("Fragment 1"));
+//        fList.add(MyFragment.newInstance("Fragment 2"));
+//        fList.add(MyFragment.newInstance("Fragment 3"));
+
+        return fList;
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        // on tab selected
+        // show respected fragment view
+        pager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+    }
+
+
+    private class MyPageAdapter extends FragmentPagerAdapter {
+        private List<Fragment> fragments;
+
+        public MyPageAdapter(FragmentManager fm, List<Fragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return this.fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return this.fragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+    }
+}
